70a84347ad320cdb24a39df84e89d2e591cecbc0
Miso Park <showmiso@gmail.com>
2020-11-22 05:17:10 +0900

    Add ViewExtension

--- class15/showmiso/DataBindingProject/app/src/main/java/com/showmiso/databindingproject/ViewExtension.kt
+++ class15/showmiso/DataBindingProject/app/src/main/java/com/showmiso/databindingproject/ViewExtension.kt
@@ -1,2 +1,9 @@
 package com.showmiso.databindingproject
 
+import android.view.View
+import androidx.databinding.BindingAdapter
+
+@BindingAdapter("isVisible")
+fun View.isVisible(visible: Boolean) {
+    visibility = if (visible) View.VISIBLE else View.GONE
+}
